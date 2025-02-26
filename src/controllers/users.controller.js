648b03ec3b30144fc86ac664db45bb2ca2c26960
Safarmurod999 <supermen0904@gmail.com>
2025-02-26 23:57:55 +0500

    users updated:

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -8,35 +8,16 @@ configDotenv();
 
 const POST = async (req, res) => {
   try {
-    const {
-      user_name,
-      first_name,
-      last_name,
-      email,
-      description_uz,
-      description_ru,
-      description_en,
-      password,
-    } = req.body;
+    const { user_name, first_name, last_name, email, password } = req.body;
     const user = await User.findOne({ where: { user_name } });
 
     if (user) {
       return res.status(400).json({ message: "User already signed up" });
     }
-    if (
-      !user_name ||
-      !first_name ||
-      !last_name ||
-      !email ||
-      !password
-      // !description_uz ||
-      // !description_ru ||
-      // !description_en
-    ) {
+    if (!user_name || !first_name || !last_name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
     const avatar = req.file;
-    const token = jwtHelper.sign({ user_name }, process.env.SECRET_KEY);
 
     const newUser = await User.create({
       user_name,
@@ -44,13 +25,9 @@ const POST = async (req, res) => {
       email: email,
       first_name,
       last_name,
-      // description_uz,
-      // description_ru,
-      // description_en,
-      avatar:
-        avatar == ""
-          ? ""
-          : `${path.join("uploads", "users", avatar?.filename)}`,
+      avatar: !avatar
+        ? ""
+        : `${path.join("uploads", "users", avatar?.filename)}`,
       is_active: true,
     });
 
@@ -173,9 +150,6 @@ const UPDATE = async (req, res) => {
       first_name,
       last_name,
       email,
-      // description_uz,
-      // description_ru,
-      // description_en,
       password,
       is_active,
     } = req.body;
@@ -200,9 +174,6 @@ const UPDATE = async (req, res) => {
         email: email ?? userData.email,
         first_name: first_name ?? userData.first_name,
         last_name: last_name ?? userData.last_name,
-        // description_uz: description_uz ?? userData.description_uz,
-        // description_ru: description_ru ?? userData.description_ru,
-        // description_en: description_en ?? userData.description_en,
         avatar: avatar
           ? `${path.join("uploads", "users", avatar.filename)}`
           : userData.avatar,
